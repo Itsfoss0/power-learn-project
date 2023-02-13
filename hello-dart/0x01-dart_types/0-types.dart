9fdadb7d9c884987b37681555dc014d8148b0ead
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-14 01:18:24 +0300

    Dart Datatypes

---
+++ hello-dart/0x01-dart_types/0-types.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  String name = "John";
+  bool isTrue = true;
+  int age = 32;
+  List names = ["John", "Doe", "Jane"];
+
+  print("=====================================================================");
+  print("Datatypes in Dart");
+  print('Strings: $name, \nBooleans: $isTrue, \nIntegers: $age, \nLists: $names');
+  print("=====================================================================");
+ 
+}
