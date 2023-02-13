22cc6ba1a1438e6db2c3ed3839853902b9d18469
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-14 01:24:32 +0300

    Added Dart types

--- hello-dart/0x01-dart_types/0-types.dart
+++ hello-dart/0x01-dart_types/0-types.dart
@@ -3,11 +3,12 @@ void main()
   String name = "John";
   bool isTrue = true;
   int age = 32;
+  double price = 22.21;
   List names = ["John", "Doe", "Jane"];
 
   print("=====================================================================");
   print("Datatypes in Dart");
-  print('Strings: $name, \nBooleans: $isTrue, \nIntegers: $age, \nLists: $names');
+  print('Strings: $name, \nBooleans: $isTrue, \nIntegers: $age, \nLists: $names, \nDobules $price');
   print("=====================================================================");
  
 }
