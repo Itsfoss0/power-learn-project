d5ed801e0dae588e7a7f785338d73c15d12d5fbd
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-22 11:52:48 +0300

    Added 1-tuple.py

---
+++ python-incubation/0x01-lists_tuples-dicts/1-tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that stores 5 of my fav books
+and prints them line by line
+"""
+
+mybooks = ("The Holy Bible",
+           "The Art of War",
+           "The 48 Laws of Power",
+           "The 48 Laws of Human Nature",
+           "The Wages of Sin")
+
+for book in mybooks:
+    print("I like reading {}".format(book))
