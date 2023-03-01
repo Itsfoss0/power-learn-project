2b6e6f16e64eb6c81912d95f8ed8522566e5b010
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-01 10:55:13 +0300

    [Enhancement]: Type casting

---
+++ hello-dart/0x01-dart_types/1-maps.dart
@@ -0,0 +1,13 @@
+/*
+Exploring maps datatype in dart
+*/
+
+void main(){
+  Map details = Map();
+  details["name"] = "John";
+  details["age"] = 22;
+  details["maried"] = false;
+  details["job"] = "DevOps";
+
+  print(details);
+}
