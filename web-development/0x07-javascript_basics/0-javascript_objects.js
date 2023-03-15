2ae6bafd8a9f62b10e5da2f52c574bcaabfbca11
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-15 14:46:19 +0300

    [chore]: Add 0-javascript

--- web-development/0x07-javascript_basics/0-javascript_objects.js
+++ web-development/0x07-javascript_basics/0-javascript_objects.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 const person = {
-    name: ["John", "Doe"],
-    age: 32,
-    bio() {
-      console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
-    },
-    introduction() {
-      console.log(`Hi! I'm ${this.name[0]}.`);
-    },
-  };
-  
+  name: ['John', 'Doe'],
+  age: 32,
+  bio () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduction () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  }
+};
+
 person.bio();
 person.introduction();
