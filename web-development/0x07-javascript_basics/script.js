ced47d0cdd9dd343a4f1836f05a256856a33afca
Ebenezer <akhonyaebenezer@gmail.com>
2023-03-21 19:09:51 +0300

    JS

--- web-development/0x07-javascript_basics/script.js
+++ web-development/0x07-javascript_basics/script.js
@@ -2,15 +2,23 @@ let totalVotes = 0;
 const votesContent = document.getElementById('votes');
 const upvoteButton = document.getElementById('upvote');
 const downvoteButton = document.getElementById('downvote');
+const question = document.getElementById('question');
+const originalQuestion = question.innerHTML;
 
 console.log(votesContent.innerHTML);
 
 upvoteButton.addEventListener('click', () => {
   totalVotes += 1;
   votesContent.textContent = `${totalVotes} Votes`;
+  if (question.textContent !== originalQuestion) {
+    question.textContent = originalQuestion;
+  }
 });
 
 downvoteButton.addEventListener('click', () => {
   totalVotes -= 1;
+  if (totalVotes < 0) {
+    question.textContent = 'Question downvoted to hell !!';
+  }
   votesContent.textContent = `${totalVotes} Votes`;
 });
