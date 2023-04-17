990519d16d7cced32d9601eb010fbd1b2ad5583b
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-17 18:00:52 +0300

    [Chore]: task one

--- python-incubation/0x07-file_io/0-words_lines_chars.py
+++ python-incubation/0x07-file_io/0-words_lines_chars.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 
+"""
+Determine number of
+lines, words and characters in the file.
+
+Usage: ./0-words_lines_chars.py <file_name>
+"""
+
+from os import path
+from sys import argv
+
+def calculate_chars_words_lines(file_name: str):
+    print("Working on this feature")
+
+
+if __name__ == "__main__":
+    if len(argv) < 2 :
+        print("Usage: ./0-words_lines_chars.py <file_name")
+    else:
+        calculate_chars_words_lines('file_name')
