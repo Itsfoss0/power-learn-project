8785cfcd92fde829e6fbd53b02561391d35711aa
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-21 18:01:27 +0300

    [Chore]: Add Base class

--- python-incubation/0x04-oop_python/shapes/shapes.py
+++ python-incubation/0x04-oop_python/shapes/shapes.py
@@ -36,6 +36,12 @@ class Shapes:
     def perimeter(self):
         """Culculate perimeter of the shape"""
 
+    @classmethod
+    def total_perimeter(cls):
+        """The total perimeters of all objects"""
+
+
+
 
 if __name__ == "__main__":
     shape_obj = Shapes(name="Circle", radius=89)
