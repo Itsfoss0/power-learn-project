462d0dbd69bfdc219192ba4250b34e982c5cf463
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-21 16:27:50 +0300

    [Chore]: Fix Base class

--- python-incubation/0x04-oop_python/shapes/shapes.py
+++ python-incubation/0x04-oop_python/shapes/shapes.py
@@ -4,6 +4,8 @@
 Shapes module
 """
 
+from uuid import uuid1
+
 class Shapes:
     """
     The Base class
@@ -12,19 +14,29 @@ class Shapes:
 
     }
 
-    def __init__(self, **kwargs):
+    def __init__(self, shape_id=uuid1(), **kwargs):
         """
         Since the Shapes class will be inherited
         We dont know what properties the subclasses will
         need to implement, we use **kwargs instead
         """
+        self.shape_id = str(shape_id)
         for key, value in kwargs.items():
             self.__setattr__(key, value)
 
-        self.__class__._objects[list(self.__dict__.values())[0]] = self.__dict__
+        self.__class__._objects[self.shape_id] = self.__dict__
+
+    def __str__(self):
+        """String representation of the object"""
+        return "[Shape] {} = {}".format(self.shape_id, self.__dict__)
 
     def area(self):
         """Culculate area of the shape"""
 
     def perimeter(self):
         """Culculate perimeter of the shape"""
+
+
+if __name__ == "__main__":
+    shape_obj = Shapes(name="Circle", radius=89)
+    print(shape_obj)
