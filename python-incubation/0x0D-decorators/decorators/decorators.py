64006da9061aeb9066a295db974abf155aa55ed4
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-19 20:31:58 +0300

    [Chore]: Add more decorators

--- python-incubation/0x0D-decorators/decorators/decorators.py
+++ python-incubation/0x0D-decorators/decorators/decorators.py
@@ -6,11 +6,14 @@ decorator to other functions.
 For the assignments at Power Learn Project
 """
 
+from time import time
+
+
 def logger(function):
     """
     Logger function: Logs a function name and parameters before calling it
     Args:
-        function: (object) the function to tbe called
+        function: (object) the function to the called
     """
 
     def wrapper(*args, **kwargs):
@@ -28,3 +31,26 @@ def logger(function):
         return return_value
 
     return wrapper
+
+
+def timer(function):
+    """
+    Timer function: times the execution time of a function
+    Args:
+        function (object) the function to be called
+    Returns:
+        returns a wrapper function (as you might have guesed)
+    """
+    
+    def wrapper(*args, **kwargs):
+        """The wrapper function"""
+        s_time = time()
+        return_value = function(*args, **kwargs)
+        e_time = time()
+        total_time = e_time - s_time
+        print(f"The function {function.__code__.co_name} took {total_time} seconds to execute.")
+
+        return return_value
+    
+    return wrapper
+
