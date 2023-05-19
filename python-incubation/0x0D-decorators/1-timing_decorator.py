19da8fdb50735738c0f2d8f0e698c4c3ecab6787
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-19 20:46:25 +0300

    [Chore]: Add timing decorator

--- python-incubation/0x0D-decorators/1-timing_decorator.py
+++ python-incubation/0x0D-decorators/1-timing_decorator.py
@@ -11,10 +11,23 @@ from time import sleep
 
 
 @timer
-def just_sleep(seconds):
+def just_sleep(seconds: int) -> int:
     """Sleep and do nothing else"""
     sleep(seconds)
 
 
+@timer
+def add_numbers(number_a: int, number_b: int) -> int:
+    """Add two integer numbers
+    Args:
+        number_a (int): First number to add
+        number_b (int): Second number to add
+    Returns
+        An integer
+    """
+    return (number_a + number_b)
+
+
 if __name__ == "__main__":
     print(just_sleep(3))
+    print(add_numbers(12, 45))
