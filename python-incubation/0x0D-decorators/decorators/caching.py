7a9e8cdc281a3d3958463d6542980811ec56c519
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-20 19:31:13 +0300

    [Chore]: Add caching decorator

---
+++ python-incubation/0x0D-decorators/decorators/caching.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Module with decorator for caching
+results of some expensive operations
+and function calls 
+"""
+
+from typing import Callable
+
+
+def cache(function: callable) -> Callable:
+    """
+    Cache expensive function's return values
+    Args:
+        function (Callable): function to cache return value
+    """
+
+    cached_items = {}
+
+    def wrapper(*args: list) -> object:
+        """The wrapper function"""
+        if args in cached_items.keys():
+            return {"cached":cached_items[args]}
+
+        return_value = function(*args)
+        cached_items[args] = return_value
+
+        return return_value
+
+    return wrapper
