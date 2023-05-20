58680791c2391ee668c350deaffa1d52531bf2c9
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-20 18:24:33 +0300

    [Lint]: Fix pycodestyle linter

--- python-incubation/0x0D-decorators/4-logging_functions.py
+++ python-incubation/0x0D-decorators/4-logging_functions.py
@@ -8,6 +8,7 @@ defined in the decorators module
 from decorators.logging import function_logger
 from decorators.decorators import validate
 
+
 @function_logger
 def make_me_awesome(*args: list, **kwargs: dict) -> list:
     """
@@ -19,6 +20,7 @@ def make_me_awesome(*args: list, **kwargs: dict) -> list:
     if args:
         return [f"{x} is awesome" for x in args]
     return [f"{name} is awesome" for name in kwargs.values()]
+
 
 @validate
 @function_logger
@@ -32,7 +34,10 @@ def add(number_a: int, number_b: int) -> int:
     """
     return (number_a + number_b)
 
+
 if __name__ == "__main__":
-    print(make_me_awesome("Jane Doe", "Kurt Weller", "Edgar Reed", "Remmy Briggs", "Keaton"))
-    print(make_me_awesome("Python", "JavaScript", "C", "Docker", "K8S", "Bash"))
-    print(add(number_a = 12, number_b = [1, 3]))
+    print(make_me_awesome("Jane Doe", "Kurt Weller", "Edgar Reed",
+          "Remmy Briggs", "Keaton"))
+    print(make_me_awesome("Python", "JavaScript", "C",
+          "Docker", "K8S", "Bash"))
+    print(add(number_a=12, number_b=[1, 3]))
