35092e522216fcb6b93988462596112570f1777c
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-21 20:21:54 +0300

    [Chore]: Add decorator utils

---
+++ python-incubation/0x0D-decorators/decorators/utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Module with utility functions to
+try out the decorator functions
+"""
+
+
+def add(first_number: int, second_number: int) -> int:
+    """
+    Add two numbers together
+    Args:
+        first_number (int): First number
+        second_number (int): Second number
+    Returns:
+        returns an integer, result of addition of the two
+    """
+
+    return (a + b)
