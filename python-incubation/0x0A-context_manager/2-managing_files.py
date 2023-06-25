b9d93dbe6a8c095758d352794857e2a54fd8f868
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-25 18:17:29 +0300

    [Chore]: Add managing files

---
+++ python-incubation/0x0A-context_manager/2-managing_files.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+Module to test out the functionality of the
+file managing context manager I've just written
+and learn more about context management in python
+"""
+
+
+from contexts import FileManager
+
+if __name__ == "__main__":
+    with FileManager("hello.txt", "a") as file:
+        file.write("Hi Mom!\n")
