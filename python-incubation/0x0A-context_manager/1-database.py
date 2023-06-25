c7cd99f3f308b13e0becd7e36fb0ba735662f13c
Ebenezer <akhonyaebenezer@gmail.com>
2023-06-25 15:49:21 +0300

    [Chore]: Add database

---
+++ python-incubation/0x0A-context_manager/1-database.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+This module test the functionality of the
+DatabaseConnection context managers I've written
+in the contexts package. Just using it for practice
+"""
+
+from contexts import DatabaseConnection
+
+if __name__ == "__main__" :
+    with DatabaseConnection('localhost', 'todoist_db', 'root', 'root') as db:
+        db.execute('SELECT name, email FROM app_users')
+        results = db.fetchall()
+        print(results)
